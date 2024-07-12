af0d12e3645b377960d261758cfb3c03bcefa20b
Romain Deville <code@romaindeville.fr>
2024-07-12 09:06:12 +0200

    ✨(plugins): Add plugin config luarocks

---
+++ lua/plugins/luarocks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priory = 1000,
+    config = true,
+    opts = {
+      rocks = {
+        "lua-curl",
+        "nvim-nio",
+        "mimetypes",
+        "xml2lua",
+      },
+    },
+  },
+}
