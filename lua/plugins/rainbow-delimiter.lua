6db9c0fcbbc97fa7d886615081925ff312e4cb5d
Romain Deville <code@romaindeville.fr>
2024-07-13 20:13:34 +0200

    ✨(plugins): Add plugin config rainbow-delimiter

---
+++ lua/plugins/rainbow-delimiter.lua
@@ -0,0 +1,16 @@
+return {
+  "hiphish/rainbow-delimiters.nvim",
+  config = function()
+    vim.g.rainbow_delimiters = {
+      highlight = {
+        "RainbowDelimiterRed",
+        "RainbowDelimiterYellow",
+        "RainbowDelimiterBlue",
+        "RainbowDelimiterOrange",
+        "RainbowDelimiterGreen",
+        "RainbowDelimiterViolet",
+        "RainbowDelimiterCyan",
+      },
+    }
+  end,
+}
