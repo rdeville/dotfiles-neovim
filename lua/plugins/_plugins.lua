044eff1db2650cfafb5c7dfdcedeeed9c5a1b5c2
Romain Deville <code@romaindeville.fr>
2024-07-13 20:10:38 +0200

    ✨(plugins): Add set of simple plugins

---
+++ lua/plugins/_plugins.lua
@@ -0,0 +1,21 @@
+return {
+  -- Plugin I use for now to setup my colorscheme
+  { "rktjmp/lush.nvim", lazy = true },
+  {
+    dir = "/home/rdeville/.config/nvim-dev/lua/colorscheme/material-dark/",
+    lazy = true,
+  },
+  -- Simple plugin without config
+  { "rhysd/committia.vim" },
+  -- Filetype Syntax
+  { "glench/vim-jinja2-syntax" },
+  { "chr4/nginx.vim" },
+  { "raimon49/requirements.txt.vim" },
+  { "dannywillems/vim-icalendar" },
+  { "fladson/vim-kitty" },
+  -- LazyVim Deactivated Plugins
+  {
+    "garymjr/nvim-snippets",
+    enabled = false,
+  },
+}
