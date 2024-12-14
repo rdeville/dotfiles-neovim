e629e642b308f2fe31a4bc35d643a5159418222c
Romain Deville <code@romaindeville.fr>
2024-12-14 18:22:53 +0100

    🔧(config): Update HighLight groups

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
+local clr = require("colorscheme.definitions")
+vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", {fg = clr.red_500.hex})
+vim.api.nvim_set_hl(0, "MiniIndentscopeSymbolOff", {fg = "None"})
+
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
