a4caae186df0d53e3b4816063f223a4ae070db4a
Romain Deville <code@romaindeville.fr>
2025-02-09 13:20:32 +0100

    🐛(plugins/explorer): Update keys to keep using NeoTree instead of snacks

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -79,4 +79,22 @@ return {
       },
     },
   },
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+  },
 }
