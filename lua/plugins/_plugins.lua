fb13c7463e5ae6492b204c99a7cb9551c6673961
Romain Deville <code@romaindeville.fr>
2025-02-09 01:31:05 +0100

    🔧(plugins): Update Lazy plugins order and add catppuccin colorscheme

--- lua/plugins/_plugins.lua
+++ lua/plugins/_plugins.lua
@@ -1,10 +1,7 @@
 return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- Plugin I use for now to setup my colorscheme
   { "rktjmp/lush.nvim", lazy = true },
-  -- {
-  --   dir = "/home/rdeville/.config/nvim-dev/lua/colorscheme/material-dark/",
-  --   lazy = true,
-  -- },
   -- Simple plugin without config
   { "rhysd/committia.vim" },
   -- Filetype Syntax
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        buffers = "transparent",
+      },
+    },
+  },
+}
