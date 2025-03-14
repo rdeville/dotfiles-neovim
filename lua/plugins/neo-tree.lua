ddef77ade7d24255812601382b153c37c2137d16
Romain Deville <code@romaindeville.fr>
2025-03-14 19:27:12 +0100

    🐛(plugins): Fix neotree plugins

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -35,7 +35,7 @@ return {
                 "notify",
                 "Outline",
                 "trouble",
-                "nofile"
+                "nofile",
               },
             },
           },
@@ -72,29 +72,93 @@ return {
     },
   },
   opts = {
+    sources = { "filesystem", "buffers", "git_status" },
     window = {
       mappings = {
         ["l"] = "open_with_window_picker",
         ["w"] = "focus_preview",
+        ["h"] = "close_node",
+        ["<space>"] = "none",
+        ["Y"] = {
+          function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            vim.fn.setreg("+", path, "c")
+          end,
+          desc = "Copy Path to Clipboard",
+        },
+        ["O"] = {
+          function(state)
+            require("lazy.util").open(
+              state.tree:get_node().path,
+              { system = true }
+            )
+          end,
+          desc = "Open with System Application",
+        },
+        ["P"] = { "toggle_preview", config = { use_float = false } },
       },
     },
-  },
-  keys = {
-    {
-      "<leader>fe",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
-      end,
-      desc = "Explorer NeoTree (Root Dir)",
+
+    open_files_do_not_replace_types = {
+      "terminal",
+      "Trouble",
+      "trouble",
+      "qf",
+      "Outline",
     },
-    {
-      "<leader>fE",
-      function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-      end,
-      desc = "Explorer NeoTree (cwd)",
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
     },
-    { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
-    { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+      git_status = {
+        symbols = {
+          unstaged = "󰄱",
+          staged = "󰱒",
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            dir = LazyVim.root(),
+          })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            dir = vim.uv.cwd(),
+          })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      {
+        "<leader>e",
+        "<leader>fe",
+        desc = "Explorer NeoTree (Root Dir)",
+        remap = true,
+      },
+      {
+        "<leader>E",
+        "<leader>fE",
+        desc = "Explorer NeoTree (cwd)",
+        remap = true,
+      },
+    },
   },
 }
