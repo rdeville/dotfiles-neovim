539132ebd6ef1edd74dab216afd17eb72ab96e6e
Romain Deville <code@romaindeville.fr>
2025-03-15 13:07:09 +0100

    🔧(plugins): Improve neo-tree configs

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,39 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          dir = LazyVim.root(),
+        })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          dir = vim.uv.cwd(),
+        })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
+    {
+      "<leader>e",
+      "<leader>fe",
+      desc = "Explorer NeoTree (Root Dir)",
+      remap = true,
+    },
+    {
+      "<leader>E",
+      "<leader>fE",
+      desc = "Explorer NeoTree (cwd)",
+      remap = true,
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -124,40 +158,6 @@ return {
           unstaged = "󰄱",
           staged = "󰱒",
         },
-      },
-    },
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          require("neo-tree.command").execute({
-            toggle = true,
-            dir = LazyVim.root(),
-          })
-        end,
-        desc = "Explorer NeoTree (Root Dir)",
-      },
-      {
-        "<leader>fE",
-        function()
-          require("neo-tree.command").execute({
-            toggle = true,
-            dir = vim.uv.cwd(),
-          })
-        end,
-        desc = "Explorer NeoTree (cwd)",
-      },
-      {
-        "<leader>e",
-        "<leader>fe",
-        desc = "Explorer NeoTree (Root Dir)",
-        remap = true,
-      },
-      {
-        "<leader>E",
-        "<leader>fE",
-        desc = "Explorer NeoTree (cwd)",
-        remap = true,
       },
     },
   },
