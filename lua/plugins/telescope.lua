24a87d9158930bcbcb352af994554f59a4cbf907
Romain Deville <code@romaindeville.fr>
2026-01-18 11:43:32 +0100

    ⚡️(lua/telescope): Configure a new layout for telescope more compact
    While debugging, I've run fzf-lua instead of telescope.
    
    I found its layout nice but prefer telescope so I search to setup a more
    compact telescope layout which I found as snippet on github (first found on
    reddit).

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: inject-field,undefined-field,param-type-mismatch
 local builtin = require("telescope.builtin")
 
 return {
@@ -7,6 +8,7 @@ return {
     dependencies = {
       -- https://github.com/nvim-lua/plenary.nvim
       "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
     },
     keys = {
       {
@@ -26,19 +28,226 @@ return {
     },
     opts = {
       defaults = {
-        layout_strategy = "vertical",
+        layout_strategy = "flex",
         layout_config = {
-          prompt_position = "bottom",
+          horizontal = {
+            size = {
+              width = "90%",
+              height = "80%",
+            },
+          },
           vertical = {
-            preview_cutoff = 20,
-            preview_height = 0.75,
-            height = 0.75,
-            width = 0.75,
-            prompt_position = "bottom",
-          };
+            size = {
+              width = "90%",
+              height = "90%",
+            },
+          },
         },
         sorting_strategy = "ascending",
         winblend = 0,
+        -- See https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#fused-layout
+        -- Below function create minimal telescope layout
+        create_layout = function(picker)
+          local TSLayout = require("telescope.pickers.layout")
+          local Layout = require("nui.layout")
+          local Popup = require("nui.popup")
+          local function make_popup(options)
+            local popup = Popup(options)
+            function popup.border:change_title(title)
+              popup.border.set_text(popup.border, "top", title)
+            end
+            return TSLayout.Window(popup)
+          end
+
+          local border = {
+            results = {
+              top_left = "┌",
+              top = "─",
+              top_right = "┬",
+              right = "│",
+              bottom_right = "",
+              bottom = "",
+              bottom_left = "",
+              left = "│",
+            },
+            results_patch = {
+              minimal = {
+                top_left = "┌",
+                top_right = "┐",
+              },
+              horizontal = {
+                top_left = "┌",
+                top_right = "┬",
+              },
+              vertical = {
+                top_left = "├",
+                top_right = "┤",
+              },
+            },
+            prompt = {
+              top_left = "├",
+              top = "─",
+              top_right = "┤",
+              right = "│",
+              bottom_right = "┘",
+              bottom = "─",
+              bottom_left = "└",
+              left = "│",
+            },
+            prompt_patch = {
+              minimal = {
+                bottom_right = "┘",
+              },
+              horizontal = {
+                bottom_right = "┴",
+              },
+              vertical = {
+                bottom_right = "┘",
+              },
+            },
+            preview = {
+              top_left = "┌",
+              top = "─",
+              top_right = "┐",
+              right = "│",
+              bottom_right = "┘",
+              bottom = "─",
+              bottom_left = "└",
+              left = "│",
+            },
+            preview_patch = {
+              minimal = {},
+              horizontal = {
+                bottom = "─",
+                bottom_left = "",
+                bottom_right = "┘",
+                left = "",
+                top_left = "",
+              },
+              vertical = {
+                bottom = "",
+                bottom_left = "",
+                bottom_right = "",
+                left = "│",
+                top_left = "┌",
+              },
+            },
+          }
+
+          local results = make_popup({
+            focusable = false,
+            border = {
+              style = border.results,
+              text = {
+                top = picker.results_title,
+                top_align = "center",
+              },
+            },
+            win_options = {
+              winhighlight = "Normal:Normal",
+            },
+          })
+
+          local prompt = make_popup({
+            enter = true,
+            border = {
+              style = border.prompt,
+              text = {
+                top = picker.prompt_title,
+                top_align = "center",
+              },
+            },
+            win_options = {
+              winhighlight = "Normal:Normal",
+            },
+          })
+
+          local preview = make_popup({
+            focusable = false,
+            border = {
+              style = border.preview,
+              text = {
+                top = picker.preview_title,
+                top_align = "center",
+              },
+            },
+          })
+
+          local box_by_kind = {
+            vertical = Layout.Box({
+              Layout.Box(preview, { grow = 1 }),
+              Layout.Box(results, { grow = 1 }),
+              Layout.Box(prompt, { size = 3 }),
+            }, { dir = "col" }),
+            horizontal = Layout.Box({
+              Layout.Box({
+                Layout.Box(results, { grow = 1 }),
+                Layout.Box(prompt, { size = 3 }),
+              }, { dir = "col", size = "50%" }),
+              Layout.Box(preview, { size = "50%" }),
+            }, { dir = "row" }),
+            minimal = Layout.Box({
+              Layout.Box(results, { grow = 1 }),
+              Layout.Box(prompt, { size = 3 }),
+            }, { dir = "col" }),
+          }
+
+          local function get_box()
+            local strategy = picker.layout_strategy
+            if strategy == "vertical" or strategy == "horizontal" then
+              return box_by_kind[strategy], strategy
+            end
+
+            local height, width = vim.o.lines, vim.o.columns
+            local box_kind = "horizontal"
+            if width < 100 then
+              box_kind = "vertical"
+              if height < 40 then
+                box_kind = "minimal"
+              end
+            end
+            return box_by_kind[box_kind], box_kind
+          end
+
+          local function prepare_layout_parts(layout, box_type)
+            layout.results = results
+            --
+            results.border:set_style(border.results_patch[box_type])
+
+            layout.prompt = prompt
+            prompt.border:set_style(border.prompt_patch[box_type])
+
+            if box_type == "minimal" then
+              layout.preview = nil
+            else
+              layout.preview = preview
+              preview.border:set_style(border.preview_patch[box_type])
+            end
+          end
+
+          local function get_layout_size(box_kind)
+            return picker.layout_config[box_kind == "minimal" and "vertical" or box_kind].size
+          end
+
+          local box, box_kind = get_box()
+          local layout = Layout({
+            relative = "editor",
+            position = "50%",
+            size = get_layout_size(box_kind),
+          }, box)
+
+          layout.picker = picker
+          prepare_layout_parts(layout, box_kind)
+
+          local layout_update = layout.update
+          function layout:update()
+            box, box_kind = get_box()
+            prepare_layout_parts(layout, box_kind)
+            layout_update(self, { size = get_layout_size(box_kind) }, box)
+          end
+
+          return TSLayout(layout)
+        end,
       },
     },
   },
